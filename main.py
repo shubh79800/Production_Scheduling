bc2c34b1bb8352eac4e0ecb945d523bc19a5665d
shubh79800 <74838408+shubh79800@users.noreply.github.com>
2023-03-10 09:49:00 -0500

    PLC

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ ipAdd = OP090.selectIP()
 FR120 = station("FR120")
 FR120ipAdd = FR120.selectIP()
 
+OP100 = station("OP100")
+OP100ipAdd = OP100.selectIP()
+
+OP120 = station("OP120")
+OP120ipAdd = OP120.selectIP()
+
 #########################################
 
 """
@@ -25,20 +31,36 @@ Element 4: Value of Tag
 
 ######################################
 while True:
-    print("START MONITORING - FR120")
-    FR120.lcTag()
-    FR120.swipeButton()
-    FR120.robotAuto()
-    FR120.robotRunning()
-    FR120.safetyGate()
+    # print("START MONITORING - FR120")
+    # FR120.lcTag()
+    # FR120.swipeButton()
+    # FR120.robotAuto()
+    # FR120.robotRunning()
+    # FR120.safetyGate()
 
-    print("START MONITORING - OP090")
+    # print("START MONITORING - OP090")
     
     # OP090.lcTag()
     # OP090.swipeButton()
     # OP090.robotAuto()
     # OP090.robotRunning()
     # OP090.safetyGate()
+
+    print("START MONITORING - FR120")
+    OP100.lcTag()
+    OP100.swipeButton()
+    OP100.robotAuto()
+    OP100.robotRunning()
+    OP100.safetyGate()
+
+    print("START MONITORING - FR120")
+    OP100.lcTag()
+    OP100.swipeButton()
+    OP100.robotAuto()
+    OP100.robotRunning()
+    OP100.safetyGate()
+
+
     print ("##################")
     time.sleep(5)
 
